2927a99e2f2ab6cf92eaa84f8302e27d9bcba62a
Ivan Malikov <ivan.malikov.2302@list.ru>
2026-03-24 17:08:15 +0300

    Все полностью связи установленны, теперь надо учить SQL чтобы писать JOIN запросы

---
+++ src/main/java/com/example/library/Entity/Loan.java
@@ -0,0 +1,35 @@
+package com.example.library.Entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "loans")
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reader_id")
+    private Reader reader;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @NotNull private LocalDate loanDate;
+    @NotNull private LocalDate duenDate;
+    @NotNull private LocalDate returnDate;
+
+}
--- src/main/java/com/example/library/Entity/Reader.java
+++ src/main/java/com/example/library/Entity/Reader.java
@@ -1,63 +1,45 @@
 package com.example.library.Entity;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "books")
+@Table(name = "readers")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Book {
+public class Reader {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank private String title;
+    @NotBlank private String firstName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
-    private Author author;
+    @NotBlank private String lasttName;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
+    @NotBlank private String email;
 
-    @Column(unique = true, nullable = false)
-    private String isbn;
-
-    @NotNull private Integer publishedYear;
-
-    @NotNull 
-    @PositiveOrZero
-    private Integer totalCopies;
-
-    @NotNull 
-    @PositiveOrZero
-    private Integer availableCopies;
-
-    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "reader", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Loan> loans = new ArrayList<>();
-    
+
+    @NotNull private LocalDate registrationDate;
 
 }
